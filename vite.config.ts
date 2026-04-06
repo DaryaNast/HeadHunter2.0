8372b13cdcbc573d85fa1e5935a3833f126c628c
Darya <nastenko_1999@mail.ru>
2026-04-06 20:48:09 +0700

    init commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Убедись, что base установлен в '/'
+  base: '/HeadHunter/',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
     assetsDir: 'assets',
   }
 })
